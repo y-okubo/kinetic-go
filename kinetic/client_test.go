e97802292bc50585902c47156981bcd4f309eccc
y-okubo <y-okubo@nekojarashi.com>
2015-11-16 23:58:58 +0900

    Add get method

--- kinetic/client_test.go
+++ kinetic/client_test.go
@@ -2,6 +2,9 @@ package kinetic
 
 import (
 	"testing"
+  "math/rand"
+  "time"
+  "strconv"
 )
 
 // go test ./kinetic
@@ -25,5 +28,30 @@ func TestDelete(t *testing.T) {
 
   if err := <-rxs; err != nil {
     t.Errorf("got %v\nwant success", err)
+  }
+}
+
+func TestGet(t *testing.T) {
+  client, _ := Connect("localhost:8123")
+  defer client.Close()
+
+  rand.Seed(time.Now().UnixNano())
+  specKey := "Key_" + strconv.FormatInt(rand.Int63(), 10)
+  specVal := "Val_" + strconv.FormatInt(rand.Int63(), 10)
+
+  rx0, _ := client.Put([]byte(specKey), []byte(specVal))
+  
+  if err := <-rx0; err != nil {
+    t.Errorf("got %v\nwant success", err)
+  }
+
+  vChan, sChan, _ := client.Get([]byte(specKey))
+  select {
+  case v := <- vChan:
+    if string(v) != specVal {
+      t.Errorf("got %v\nwant %v", string(v), string(specVal))
+    }
+  case s := <- sChan:
+    t.Errorf("got %v\nwant success", s)
   }
 }
